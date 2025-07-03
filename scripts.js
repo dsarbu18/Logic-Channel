37d22938cf681f03fa6340a830f87f9f5e932e95
dsarbu18 <4dennis2go@gmail.com>
2025-07-03 12:22:56 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -70,7 +70,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menu-toggle");
   const nav = document.getElementById("main-nav");
 
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
 });
